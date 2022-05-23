45b4e9c1082ae11754da880675175c51c029ca7d
dekanymate <dekanymate@gmail.com>
2022-05-23 12:36:52 +0200

    tesztloves()

--- src/main/torpedoteszt.java
+++ src/main/torpedoteszt.java
@@ -4,8 +4,13 @@ package main;
 public class torpedoteszt {
     public static void main(String[] args) {
         System.out.println("Teszt");
+        
+        new torpedoteszt().tesztLoves(4);
     }
     public String tesztLoves(int poz){
+        Hajo hajo = new Hajo();
+        String t = hajo.talalat(poz);
+        assert t.equals("Talűlt") : "Nem jó a találat ellneőrzése";
         return "";
     }
 }
