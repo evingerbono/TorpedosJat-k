0fcb3423cdc4fbc11be1b44a19884ac45907a976
dekanymate <dekanymate@gmail.com>
2022-05-23 12:19:20 +0200

    TorpedoTeszt

---
+++ src/main/Torpedo.java
@@ -0,0 +1,10 @@
+
+package main;
+
+public class Torpedo {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/main/torpedoteszt.java
@@ -0,0 +1,11 @@
+
+package main;
+
+public class torpedoteszt {
+    public static void main(String[] args) {
+        System.out.println("Teszt");
+    }
+    public String tesztLoves(int poz){
+        return "";
+    }
+}
